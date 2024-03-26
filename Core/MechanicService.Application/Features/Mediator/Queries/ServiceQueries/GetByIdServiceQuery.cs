b16cd64239e77ba5560409628fcb8f4e98b0a30a
alicansariboga <alicnn38@gmail.com>
2024-03-26 12:14:28 +0300

    usings.

--- Core/MechanicService.Application/Features/Mediator/Queries/ServiceQueries/GetByIdServiceQuery.cs
+++ Core/MechanicService.Application/Features/Mediator/Queries/ServiceQueries/GetByIdServiceQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MechanicService.Application.Features.Mediator.Queries.ServiceQueries
+﻿namespace MechanicService.Application.Features.Mediator.Queries.ServiceQueries
 {
     public class GetByIdServiceQuery : IRequest<GetByIdServiceQueryResult>
     {
