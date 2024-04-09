8b16572229759ab7b92af947b8ecc373235233b4
alicansariboga <alicnn38@gmail.com>
2024-04-10 01:30:54 +0300

    Home page banner component partial.

--- Presentation/MechanicService.WebUI/ViewComponents/DefaultViewComponents/_DefaultBannerComponentPartial.cs
+++ Presentation/MechanicService.WebUI/ViewComponents/DefaultViewComponents/_DefaultBannerComponentPartial.cs
@@ -1,9 +1,27 @@
-﻿namespace MechanicService.WebUI.ViewComponents.DefaultViewComponents
+﻿using MechanicService.Dto.BannerDtos;
+using Newtonsoft.Json;
+
+namespace MechanicService.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultBannerComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultBannerComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
