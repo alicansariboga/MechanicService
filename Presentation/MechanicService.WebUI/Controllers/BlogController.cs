1f235b6690ae221dea9f5e0f3b6ab63f3a87091c
alicansariboga <alicnn38@gmail.com>
2024-04-13 02:49:28 +0300

    Blog page.

--- Presentation/MechanicService.WebUI/Controllers/BlogController.cs
+++ Presentation/MechanicService.WebUI/Controllers/BlogController.cs
@@ -1,9 +1,26 @@
-﻿namespace MechanicService.WebUI.Controllers
+﻿using MechanicService.Dto.BlogDtos;
+
+namespace MechanicService.WebUI.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Blogs");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
