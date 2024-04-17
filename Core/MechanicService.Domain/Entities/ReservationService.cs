48562c2adc2dfb67ec28d94ff62057a71145a7d8
alicansariboga <alicnn38@gmail.com>
2024-04-17 09:37:05 +0300

    db reservaton service time process.

--- Core/MechanicService.Domain/Entities/ReservationService.cs
+++ Core/MechanicService.Domain/Entities/ReservationService.cs
@@ -1,10 +1,16 @@
-﻿namespace MechanicService.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MechanicService.Domain.Entities
 {
     public class ReservationService : BaseEntity
     {
         public string City { get; set; }
         public string Distinct { get; set; }
+
+        [Column(TypeName = "Date")]
         public DateTime Date { get; set; }
+
+        [DataType(DataType.Time)]
         public DateTime Hour { get; set; }
         public string? Description { get; set; }
     }
---
+++ Infrastructure/MechanicService.Persistence/Migrations/20240417063250_mig_add_07_reservationService_date.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MechanicService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_07_reservationService_date : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "ReservationServices",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "ReservationServices",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+        }
+    }
+}
