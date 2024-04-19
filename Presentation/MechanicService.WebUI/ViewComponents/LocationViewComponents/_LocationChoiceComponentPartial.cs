aad7d4cb5ab228e762ba418dd0ded590a9d55c38
alicansariboga <alicnn38@gmail.com>
2024-04-19 08:47:55 +0300

    Controller fixing in Location Page.

--- Presentation/MechanicService.WebUI/ViewComponents/LocationViewComponents/_LocationChoiceComponentPartial.cs
+++ Presentation/MechanicService.WebUI/ViewComponents/LocationViewComponents/_LocationChoiceComponentPartial.cs
@@ -12,23 +12,15 @@
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var locationViewModel = new LocationsViewModel();
 
             var responseMessage = await client.GetAsync("https://localhost:7215/api/LocationCities");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                locationViewModel.CityDatas = JsonConvert.DeserializeObject<List<ResultLocationCityDto>>(jsonData);
+                var results = JsonConvert.DeserializeObject<List<ResultLocationCityDto>>(jsonData);
+                return View(results);
             }
-
-            var responseMessage2 = await client.GetAsync($"https://localhost:7215/api/LocationDistricts/GetLocationDistrictsByCityId?id=");
-            if (responseMessage2.IsSuccessStatusCode)
-            {
-                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
-                locationViewModel.DistrictDatas = JsonConvert.DeserializeObject<List<ResultLocationDistrictDto>>(jsonData2);
-            }
-
-            return View(locationViewModel);
+            return View();
         }
 
         [HttpGet]
