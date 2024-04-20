b210f7dfb6e49d92dfd993001ae1d3707f43f8c5
alicansariboga <alicnn38@gmail.com>
2024-04-21 00:41:50 +0300

    Reservation Sending is complete.

--- Presentation/MechanicService.WebUI/Controllers/ReservationController.cs
+++ Presentation/MechanicService.WebUI/Controllers/ReservationController.cs
@@ -1,5 +1,4 @@
-﻿using MechanicService.Dto.BannerDtos;
-using System.Text;
+﻿using System.Text;
 
 namespace MechanicService.WebUI.Controllers
 {
@@ -13,62 +12,41 @@ namespace MechanicService.WebUI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var client = _httpClientFactory.CreateClient();
-
             var viewRezervationModel = new ReservationCreateViewModel
             {
                 ReservationCar = new CreateReservationCarDto(),
                 ReservationPerson = new CreateReservationPersonDto(),
                 ReservationService = new CreateReservationServiceDto()
             };
-            var viewCarBrand = new CarBrandViewModel();
-            var viewLocations = new LocationsViewModel();
-            var combinedViewModel = new CombinedCarViewModel
-            {
-                CarBrandViewModel = viewCarBrand,
-                ReservationCreateViewModel = viewRezervationModel,
-                LocationsViewModel = viewLocations
-            };
 
-            var responseMessage1 = await client.GetAsync("https://localhost:7215/api/CarBrands");
-            if (responseMessage1.IsSuccessStatusCode)
-            {
-                var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
-                combinedViewModel.CarBrandViewModel.BrandDatas = JsonConvert.DeserializeObject<List<ResultCarBrandDto>>(jsonData1);
-                //View(viewCarBrand.BrandDatas); 
-            }
-
-            var responseMessage2 = await client.GetAsync("https://localhost:7215/api/LocationCities");
-            if (responseMessage2.IsSuccessStatusCode)
-            {
-                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
-                combinedViewModel.LocationsViewModel.CityDatas = JsonConvert.DeserializeObject<List<ResultLocationCityDto>>(jsonData2);
-                //return View(viewLocations.CityDatas);
-            }
-
-            return View(combinedViewModel);
+            return View(viewRezervationModel);
+            //return View();
         }
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Index(ReservationCreateViewModel reservationModel)
+        public async Task<IActionResult> Index(ReservationCreateViewModel reservationCreateViewModel)
         {
             var client = _httpClientFactory.CreateClient();
 
-            var jsonData1 = JsonConvert.SerializeObject(reservationModel.ReservationPerson);
+            var jsonData1 = JsonConvert.SerializeObject(reservationCreateViewModel.ReservationPerson);
             StringContent stringContent1 = new StringContent(jsonData1, Encoding.UTF8, "application/json");
-            await client.PostAsync("https://localhost:7215/api/ReservationPersons", stringContent1);
+            var responseMessage1 =  await client.PostAsync("https://localhost:7215/api/ReservationPersons", stringContent1);
 
-            var jsonData2 = JsonConvert.SerializeObject(reservationModel.ReservationCar);
+            var jsonData2 = JsonConvert.SerializeObject(reservationCreateViewModel.ReservationCar);
             StringContent stringContent2 = new StringContent(jsonData2, Encoding.UTF8, "application/json");
-            await client.PostAsync("https://localhost:7215/api/ReservationCars", stringContent2);
+            var responseMessage2 = await client.PostAsync("https://localhost:7215/api/ReservationCars", stringContent2);
 
-            var jsonData3 = JsonConvert.SerializeObject(reservationModel.ReservationService);
+            var jsonData3 = JsonConvert.SerializeObject(reservationCreateViewModel.ReservationService);
             StringContent stringContent3 = new StringContent(jsonData3, Encoding.UTF8, "application/json");
-            await client.PostAsync("https://localhost:7215/api/ReservationServices", stringContent3);
+            var responseMessage3 = await client.PostAsync("https://localhost:7215/api/ReservationServices", stringContent3);
 
-            return RedirectToAction("Index", "ReservationComplete");
+            if (responseMessage1.IsSuccessStatusCode && responseMessage2.IsSuccessStatusCode && responseMessage3.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "ReservationComplete");
+            }
+
+            return View();
         }
     }
 }
