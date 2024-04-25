7af908b50202ef20e7fbd8ee4d82e825e4f44594
alicansariboga <alicnn38@gmail.com>
2024-04-26 01:46:23 +0300

    Inbox main.

--- Presentation/MechanicService.WebUI/Areas/Admin/Controllers/InboxController.cs
+++ Presentation/MechanicService.WebUI/Areas/Admin/Controllers/InboxController.cs
@@ -1,12 +1,63 @@
-﻿namespace MechanicService.WebUI.Areas.Admin.Controllers
+﻿using System.Globalization;
+namespace MechanicService.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("Admin/Inbox")]
     public class InboxController : Controller
     {
-        [Route("Index")]
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public InboxController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+
+                //// Date Ago
+                //DateTime sendDate;
+                //if (DateTime.TryParseExact(values.SendDate.ToString(), "yyyy-MM-dd HH:mm:ss", CultureInfo.CurrentCulture, DateTimeStyles.None, out sendDate))
+                //{
+                //    var now = DateTime.Now;
+
+                //    var timeDifference = now - sendDate;
+                //    string timeAgo;
+
+                //    if (timeDifference.TotalDays < 1)
+                //    {
+                //        if (timeDifference.TotalHours < 1)
+                //        {
+                //            timeAgo = $"{(int)timeDifference.TotalMinutes} dakika önce";
+                //        }
+                //        else
+                //        {
+                //            timeAgo = $"{(int)timeDifference.TotalHours} saat önce";
+                //        }
+                //    }
+                //    else if (timeDifference.TotalDays < 7)
+                //    {
+                //        timeAgo = $"{(int)timeDifference.TotalDays} gün önce";
+                //    }
+                //    else
+                //    {
+                //        timeAgo = sendDate.ToString("dd.MM.yyyy");
+                //    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Tarih/saat biçimi geçersiz!");
+                //}
+
+                return View(values);
+            }
             return View();
         }
         [Route("MailDetail")]
