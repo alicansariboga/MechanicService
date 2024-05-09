fd20aad7e49484917f9a8ca8bc6fb0314230c469
alicansariboga <alicnn38@gmail.com>
2024-05-09 11:49:01 +0300

    General revision of about and default pages.

--- Presentation/MechanicService.WebUI/Controllers/AboutController.cs
+++ Presentation/MechanicService.WebUI/Controllers/AboutController.cs
@@ -1,10 +1,29 @@
-﻿namespace MechanicService.WebUI.Controllers
+﻿using MechanicService.Dto.AboutDtos;
+
+namespace MechanicService.WebUI.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Abouts/1"); // ID number of the About API = 1
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultAboutDto>(jsonData);
+                return View(values);
+            }
             return View();
         }
+
     }
 }
