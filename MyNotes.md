180bb165b9def1106655c84aa81fefc3553a5a9d
alicansariboga <alicnn38@gmail.com>
2024-05-25 15:41:55 +0300

    Customer informations.

--- MyNotes.md
+++ MyNotes.md
@@ -1,32 +1,27 @@
 ## MY NOTES
 
-- The entities have been created, but I am not sure about the reservation part distinction I made. Therefore, the reservation entity list can be updated later. A more accurate path is chosen during API tests and linq queries.
-- 3 campaign header information has been added to the campaign entity. Instead, a more dynamic title system can be added. This section will also be revised in the future.
+	- The entities have been created, but I am not sure about the reservation part distinction I made. Therefore, the reservation entity list can be updated later. A more accurate path is chosen during API tests and linq queries.
+	- 3 campaign header information has been added to the campaign entity. Instead, a more dynamic title system can be added. This section will also be revised in the future.
 
 ## REVISIONS
 
-- The name of the district property in the reservation service entity is spelled incorrectly. It will be corrected.
+	- The name of the district property in the reservation service entity is spelled incorrectly. It will be corrected.
 
-- Entity connection will be made to reservation services for service information.
+	- Entity connection will be made to reservation services for service information.
 
-- User confirmation attribute will be added for reservation entity.
+	- User confirmation attribute will be added for reservation entity.
 
-- Entity and API operations were not performed for Contact. These will also be added.
+	- Entity and API operations were not performed for Contact. These will also be added.
 
-- Reservation inquiry screen for users.
-
+	- Reservation inquiry screen for users.
 
 
 ## NEW SIDE NOTES -- 17.05.2024
 
 	- Admin Rezervasyon sayfasý detay iþlemleri yapýlacak.
 	+ Admin Þube sayfasý için aktif þube sayýsý güncellenecek.
-	- Admin Sisteme kullanýcý ekleme sayfasý yapýlacak.
+	+ Admin Sisteme kullanýcý ekleme sayfasý yapýlacak.
 	+ Admin Dashboard sayfasýnda Arabalar güncellenecek.
 	+ UI tarafýnda araç hizmeteleri ve detaylarý eklenecek.
 	- Admin Toplu Mail gönderme ve SMS gönderme sayfalarý yapýlacak. (Optional)
-
-
-
-
 
--- Presentation/MechanicService.WebUI/Areas/Admin/Controllers/ReservationController.cs
+++ Presentation/MechanicService.WebUI/Areas/Admin/Controllers/ReservationController.cs
@@ -220,5 +220,112 @@ namespace MechanicService.WebUI.Areas.Admin.Controllers
             }
             return View(reservationAllViewModel);
         }
+        [HttpGet]
+        [Route("CustomerInformation/{id}")]
+        public async Task<IActionResult> CustomerInformation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var reservationInformationViewModel = new ReservationInformationViewModel
+            {
+                ReservationPerson = new ResultReservationPersonDto(),
+
+            };
+
+            var responseMessage = await client.GetAsync($"https://localhost:7215/api/Reservations/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var reservation = JsonConvert.DeserializeObject<ResultReservationDto>(jsonData);
+
+                var singleRezPersonId = reservation.RezPersonID;
+
+                var responsePerson = await client.GetAsync($"https://localhost:7215/api/ReservationPersons/{singleRezPersonId}");
+                if (responsePerson.IsSuccessStatusCode)
+                {
+                    var personData = await responsePerson.Content.ReadAsStringAsync();
+                    var person = JsonConvert.DeserializeObject<ResultReservationPersonDto>(personData);
+
+                    reservationInformationViewModel.ReservationPerson = person;
+                }
+            }
+            return View(reservationInformationViewModel);
+
+        }
+        [HttpGet]
+        [Route("ReservationInformation")]
+        public async Task<IActionResult> ReservationInformation()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var reservationAllViewModel = new ReservationAllViewModel
+            {
+                ReservationPerson = new ResultReservationPersonDto(),
+                ReservationCar = new ResultReservationCarDto(),
+                ReservationService = new ResultReservationServiceDto(),
+                Reservations = new List<ResultReservationDto>(),
+                ReservationCombined = new List<ResultReservationAllDto>(),
+
+            };
+
+            var responseMessage = await client.GetAsync("https://localhost:7215/api/Reservations/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var reservations = JsonConvert.DeserializeObject<List<ResultReservationDto>>(jsonData);
+
+                foreach (var reservation in reservations)
+                {
+                    var singleRezPersonId = reservation.RezPersonID;
+                    var singleRezCarId = reservation.RezCarID;
+                    var singleRezServiceId = reservation.RezServiceID;
+
+                    if (reservation.IsApproved == true)
+                    {
+                        var responsePerson = await client.GetAsync($"https://localhost:7215/api/ReservationPersons/{singleRezPersonId}");
+                        if (responsePerson.IsSuccessStatusCode)
+                        {
+                            var personData = await responsePerson.Content.ReadAsStringAsync();
+                            var person = JsonConvert.DeserializeObject<ResultReservationPersonDto>(personData);
+                            reservationAllViewModel.ReservationPerson.Name = person.Name;
+                            reservationAllViewModel.ReservationPerson.Surname = person.Surname;
+                        }
+
+                        var responseCar = await client.GetAsync($"https://localhost:7215/api/ReservationCars/{singleRezCarId}");
+                        if (responseCar.IsSuccessStatusCode)
+                        {
+                            var carData = await responseCar.Content.ReadAsStringAsync();
+                            var car = JsonConvert.DeserializeObject<ResultReservationCarDto>(carData);
+                            reservationAllViewModel.ReservationCar.LicensePlate = car.LicensePlate;
+                        }
+
+                        var responseService = await client.GetAsync($"https://localhost:7215/api/ReservationServices/{singleRezServiceId}");
+                        if (responseService.IsSuccessStatusCode)
+                        {
+                            var serviceData = await responseService.Content.ReadAsStringAsync();
+                            var service = JsonConvert.DeserializeObject<ResultReservationServiceDto>(serviceData);
+                            reservationAllViewModel.ReservationService.Date = service.Date;
+                            reservationAllViewModel.ReservationService.Hour = service.Hour;
+                        }
+
+                        var combinedReservation = new ResultReservationAllDto
+                        {
+                            RezServiceID = reservation.RezServiceID,
+                            RezCarID = reservation.RezCarID,
+                            RezPersonID = reservation.RezPersonID,
+                            IsApproved = reservation.IsApproved,
+                            CreateDate = reservation.CreateDate,
+                            Id = reservation.Id,
+                            IsCanceled = reservation.IsCanceled,
+                            PersonName = reservationAllViewModel.ReservationPerson.Name,
+                            PersonSurname = reservationAllViewModel.ReservationPerson.Surname,
+                            LicensePlate = reservationAllViewModel.ReservationCar.LicensePlate,
+                            ReservationDate = reservationAllViewModel.ReservationService.Date,
+                            ReservationHour = reservationAllViewModel.ReservationService.Hour,
+                        };
+                        reservationAllViewModel.ReservationCombined.Add(combinedReservation);
+                    }
+                }
+            }
+            return View(reservationAllViewModel);
+        }
     }
 }
